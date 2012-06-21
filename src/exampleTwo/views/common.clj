319b111c7367fc11a8c2335d5ab786ecc24d9d26
Gereon Frey <me@gereonfrey.de>
2012-06-21 21:00:44 +0200

    improved the common layout
    This is shown of each page (a "frame") and now contains links
    to the register and login/logout page.

--- src/exampleTwo/views/common.clj
+++ src/exampleTwo/views/common.clj
@@ -1,4 +1,5 @@
 (ns exampleTwo.views.common
+  (:require [noir.session :as session])
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-css html5]]))
 
@@ -8,5 +9,10 @@
                [:title "exampleTwo"]
                (include-css "/css/reset.css")]
               [:body
+               [:nav
+                [:a {:href "/register"} "register"] " "
+                (if-let [user (session/get :user)]
+                  [:a {:href "/logout"} "logout"]
+                  [:a {:href "/login"} "login"])]
                [:div#wrapper
                 content]]))
