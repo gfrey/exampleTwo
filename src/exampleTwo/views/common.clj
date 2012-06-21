93fc2a092ddba828872da32f3f1b425af3928c6e
Gereon Frey <me@gereonfrey.de>
2012-06-21 18:18:40 +0200

    initial commit

---
+++ src/exampleTwo/views/common.clj
@@ -0,0 +1,12 @@
+(ns exampleTwo.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page-helpers :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "exampleTwo"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
---
+++ src/exampleTwo/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns exampleTwo.views.welcome
+  (:require [exampleTwo.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to exampleTwo"]))
