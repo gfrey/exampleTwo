4021d6c28ba1c7cd8c85ee2c670f64271b8dcbd5
Gereon Frey <me@gereonfrey.de>
2012-06-21 18:20:28 +0200

    added stupiddb dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject exampleTwo "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [stupiddb "0.4.0"]]
             :main exampleTwo.server)
 
