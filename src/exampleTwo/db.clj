1bb96bc767ef3aa148b99ac5fe094056df58fc2a
Gereon Frey <me@gereonfrey.de>
2012-06-21 20:45:02 +0200

    added support for a database

---
+++ src/exampleTwo/db.clj
@@ -0,0 +1,29 @@
+(ns exampleTwo.db
+  "A namespace containing the basic database adapter."
+  (:require [stupiddb.core :as db]))
+
+(def db (atom nil))
+
+(defn init
+  "Initializes the database and keeps it available for usage. Keep in
+   mind that for stupiddb there must only be one instance opened at a
+   time!"
+  [path]
+  (compare-and-set! db nil (db/db-init path 30)))
+
+(defn read 
+  "Read the document with the given id."
+  ([type]
+     (db/db-get-in @db [type]))
+  ([type id]
+     (db/db-get-in @db [type id])))
+
+(defn create
+  "Create the given document (with the _id field being used as id)."
+  [{:keys [type id] :as doc}]
+  (db/db-assoc-in @db [type id] doc))
+
+(defn delete
+  "Delete the document with the given id."
+  [type id]
+  (db/db-dissoc-in @db [type id]))
--- src/exampleTwo/server.clj
+++ src/exampleTwo/server.clj
@@ -1,11 +1,13 @@
 (ns exampleTwo.server
-  (:require [noir.server :as server]))
+  (:require [exampleTwo.db :as db]
+            [noir.server :as server]))
 
 (server/load-views "src/exampleTwo/views/")
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (db/init "database")
     (server/start port {:mode mode
                         :ns 'exampleTwo})))
 
